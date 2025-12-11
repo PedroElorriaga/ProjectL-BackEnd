ed2022a79d4c902a481728cbec60d18ed33c32bf
PedroElorriaga <pedrohes2002@outlook.com>
2025-12-11 12:53:41 -0300

    fix: roolback settings

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from src.models.mysql.settings.mysql_model import db
 if __name__ == '__main__':
     app = create_app()
 
-    # with app.app_context():
-    #     db.create_all()
-
-    # db.create_all()
+    with app.app_context():
+        db.create_all()
 
     app.run(port=3000, debug=True, host='0.0.0.0')
