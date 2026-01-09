57594b36e7465bf8b76062ed1daccca738b8846a
PedroElorriaga <pedrohes2002@outlook.com>
2026-01-09 19:55:33 -0300

    Feat: new instance method to create db using Postgres

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from src.main.server.server import create_app
-from src.models.mysql.settings.mysql_model import db
+from src.databases.postgres.settings.postgres_config import PostgresDbAlchemy
+from src.databases.postgres.model.catalog import Catalog
+from src.databases.postgres.model.user import User
 
 app = create_app()
+db = PostgresDbAlchemy.db
 
 with app.app_context():
     db.create_all()
