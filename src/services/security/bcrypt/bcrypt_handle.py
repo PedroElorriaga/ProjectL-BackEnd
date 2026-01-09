e5e5588d0934e13c3bc4af6e86430b3f494a22c5
PedroElorriaga <pedrohes2002@outlook.com>
2026-01-09 19:58:46 -0300

    Fix: return method type decoded to use str and attribute in check method converted to str

--- src/services/security/bcrypt/bcrypt_handle.py
+++ src/services/security/bcrypt/bcrypt_handle.py
@@ -4,10 +4,10 @@ import bcrypt
 class BcryptHandle:
 
     @staticmethod
-    def hash_content(input: str) -> bytes:
+    def hash_content(input: str) -> str:
         hashed_input = bcrypt.hashpw(input.encode('utf-8'), bcrypt.gensalt())
-        return hashed_input
+        return hashed_input.decode('utf-8')
 
     @staticmethod
-    def check_content(input: str, stored_hash: bytes) -> bool:
+    def check_content(input: str, stored_hash: str) -> bool:
         return bcrypt.checkpw(input.encode('utf-8'), stored_hash.encode('utf-8'))
