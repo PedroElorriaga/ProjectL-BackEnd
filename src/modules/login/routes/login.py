ef2707f174dbd0baf425a9a423f1b58de113d9dd
PedroElorriaga <pedrohes2002@outlook.com>
2026-03-09 17:17:26 -0300

    feat: new features implemented

--- src/modules/login/routes/login.py
+++ src/modules/login/routes/login.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.shared.http_types.http_requests import HttpRequest
 from src.modules.login.composers.login_composer import login_composer
+from src.services.security.rate_limiter.rate_limiter import rate_limiter
 
 
 login_route = Blueprint('login', __name__)
@@ -8,6 +9,7 @@ login_route = Blueprint('login', __name__)
 
 @login_route.route('/', defaults={'id_credential': None}, methods=['POST'])
 @login_route.route('/<int:id_credential>', methods=['POST'])
+@rate_limiter.limit(max_requests=5, window_seconds=60)
 def make_login(id_credential: int | None = None) -> jsonify:
     http_request = HttpRequest(request.json)
     login_repo = login_composer()
